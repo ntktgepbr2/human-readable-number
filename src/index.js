e05fad558b47890536fa5ab4f083c98de5212a70
ntktgepbr2 <53575596+ntktgepbr2@users.noreply.github.com>
2020-03-22 21:46:49 +0300

    feat: solve human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+    let tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    function numberToReadable(n) {
+        if (n < 20) return num[n];
+        let digit = n % 10;
+        if (n < 100)
+            return (
+                tens[Math.floor(n / 10) - 2] + (digit ? " " + num[digit] : "")
+            );
+        if (n < 1000)
+            return (
+                num[Math.floor(n / 100)] +
+                " hundred" +
+                (n % 100 == 0 ? "" : " " + numberToReadable(n % 100))
+            );
+        return (
+            numberToReadable(Math.floor(n / 1000)) +
+            " thousand" +
+            (n % 1000 != 0 ? " " + numberToReadable(n % 1000) : "")
+        );
+    }
+    return numberToReadable(number);
+};
